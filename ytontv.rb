1676356219c68474958100578a2659b1c9d2bf96
mattruzzi <68619790+mattruzzi@users.noreply.github.com>
2020-11-27 18:47:59 -0800

    v11.0.2,11.0.3 Nativefier 11.0.2, Electron 11.0.3

--- ytontv.rb
+++ ytontv.rb
@@ -1,13 +1,15 @@
 cask "ytontv" do
-  version "10.1.5,10.1.5"
-  sha256 "2298a6d6e170290a25867538a4d67225e5683ee0190b9a20e45ce38c747c86b6"
+  version "11.0.2,11.0.3"
+  sha256 "4fab8ee2758c170a8f0eab7207930fcb5c05c6e4140347c66a54911d51d558e8"
 
   url "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases/download/#{version}/YouTubeonTV-darwin-x64.zip"
+  appcast "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases.atom"
   name "YouTube on TV.app"
   desc "Electron/Nativefier wrapper for YouTube.com/tv (YouTube Smart TV interface)"
   homepage "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop"
-  appcast 'https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases.atom'
-  app "YouTube on TV-darwin-x64/YouTube on TV.app"
+
+  app "YouTubeonTV-darwin-x64/YouTube\ on\ TV.app"
+
   postflight do
     system "xattr -r -d com.apple.quarantine '#{appdir}/YouTube\ on\ TV.app'"
   end
