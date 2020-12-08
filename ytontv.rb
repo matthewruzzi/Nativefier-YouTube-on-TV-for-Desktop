47c893bd1f1b364a8cc4faf733fddb03004dc684
mattruzzi <68619790+mattruzzi@users.noreply.github.com>
2020-12-07 17:47:37 -0800

    v42.0.2,11.0.4 Nativefier 42.0.2, Electron 11.0.4

--- ytontv.rb
+++ ytontv.rb
@@ -1,12 +1,12 @@
-cask "ytontv" do
-  version "11.0.2,11.0.3"
-  sha256 "4fab8ee2758c170a8f0eab7207930fcb5c05c6e4140347c66a54911d51d558e8"
+cask 'ytontv' do
+  version '42.0.2,11.0.4'
+  sha256 '0e2a5f1e2f7038a1a3e031ee094d465e1d349a7a4f46b80bae9a9374ec1ba887'
 
   url "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases/download/#{version}/YouTubeonTV-darwin-x64.zip"
-  appcast "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases.atom"
-  name "YouTube on TV.app"
-  desc "Electron/Nativefier wrapper for YouTube.com/tv (YouTube Smart TV interface)"
-  homepage "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop"
+  appcast 'https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases.atom'
+  name 'YouTube on TV.app'
+  desc 'Electron/Nativefier wrapper for YouTube.com/tv (YouTube Smart TV interface)'
+  homepage 'https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop'
 
   app "YouTubeonTV-darwin-x64/YouTube\ on\ TV.app"
 
